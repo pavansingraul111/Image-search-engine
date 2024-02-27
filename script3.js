23668200283ef5ae36516e6cb9f28f2415c1b45f
pavansingraul111 <157712469+pavansingraul111@users.noreply.github.com>
2024-02-27 10:30:05 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,45 @@
+const accessKey = "nD6Rl2toWHeJF4UcEuEK3ccICOGJMA8bPpOZta0QlxY"
+
+const inputText = document.getElementById("input-text")
+const searchBtn = document.getElementById("btn")
+const loadMoreBtn = document.getElementById("load-more-btn")
+const imgSection = document.getElementById("img-section")
+
+ let keyword = "";
+ let page = 1;
+
+async function searchImages(){
+    keyword = inputText.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    if(page === 1){
+        imgSection.innerHTML = "";
+    }
+
+    const results = data.results;
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.appendChild(image);
+        imgSection.appendChild(imageLink) 
+    })
+    loadMoreBtn.style.display = "block";
+}
+searchBtn.addEventListener("click", (e) =>{
+    e.preventDefault;
+    page = 1;
+    searchImages();
+})
+loadMoreBtn.addEventListener("click", (e) =>{
+    page++;
+    searchImages();
+})
+inputText.addEventListener("keypress", function(e) {
+    if(e.key === "Enter"){
+        searchImages();
+    }
+})
